76c6cb27be0c1c52f43d60b7959a367bc029e41e
Likit Preeyanon <preeyano@msu.edu>
2015-06-09 04:26:39 -0400

    used for loop to submit jobs

--- fastqc.sh
+++ fastqc.sh
@@ -1,4 +1,4 @@
-for f in data/SRR534006_?.fastq.gz
-do
-     fastqc -o fastqc_output -f fastq --noextract -t 4 $f
+ for f in data/*.fastqc.gz
+ do
+     qsub -v 'input=$f' protocol/fastqc_job.sh
  done
