ac648e683817ddd0de1d46c2286b5f46dc14fa7a
Likit Preeyanon <preeyano@msu.edu>
2015-06-11 00:16:04 -0400

    fixed bug in trimmomatic job
    replaced hardcoded with variables
    
    added more variables in trimmomatic run

--- trimmomatic_job.sh
+++ trimmomatic_job.sh
@@ -7,7 +7,7 @@
 cd ${PBS_O_WORKDIR}
 module load Trimmomatic/0.32
 first=${input}
-first=$(basename $f .fastq.gz)
+first=$(basename $first .fastq.gz)
 second=$(echo $first | sed s/_1/_2/)
 
-java -jar $TRIM/trimmomatic PE -threads 4 $first.fastq.gz $second.fastq.gz trimmed_data/$first.trimmed.pe.fastq.gz trimmed_data/$second.trimmed.pe.fastq.gz trimmed_data/$first.trimmed.se.fastq.gz trimmed_data/$second.trimmed.se.fastq.gz ILLUMINACLIP:/mnt/research/common-data/Bio/Trimmomatic/adapters/TruSeq3-PE.fa:2:30:10
+java -jar $TRIM/trimmomatic PE -threads 4 ${data_dir}/$first.fastq.gz ${data_dir}/$second.fastq.gz ${out_dir}/$first.trimmed.pe.fastq.gz ${out_dir}/$second.trimmed.pe.fastq.gz ${out_dir}/$first.trimmed.se.fastq.gz ${out_dir}/$second.trimmed.se.fastq.gz ILLUMINACLIP:/mnt/research/common-data/Bio/Trimmomatic/adapters/TruSeq3-PE.fa:2:30:10
--- trimmomatic_run.sh
+++ trimmomatic_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for f in data/SRR536787_1.fastq.gz
 do
-    qsub -v input="$f" protocol/trimmomatic_job.sh
+    qsub -v input="$f",out_dir="trimmed_data",data_dir="data" protocol/trimmomatic_job.sh
 done
